6fd1d4a39e896a449d3d6654b29ae54825286348
Andrés Odio <andresodio@gmail.com>
2015-04-12 23:38:04 +0100

    Docs: Add Doxygen comments to liblager_connect

--- liblager_connect/liblager_connect.h
+++ liblager_connect/liblager_connect.h
@@ -18,11 +18,17 @@ using std::vector;
  * Describes the structure of a subscribed gesture
  */
 struct SubscribedGesture {
+  /// Name of the gesture
   string name;
+  /// LaGeR string representing the gesture
   string lager;
+  /// LaGeR string expanded to LCM of all gestures
   string expanded_lager;
+  /// PID of the subscribing process
   pid_t pid;
+  /// Distance between this gesture and the current input
   int distance;
+  /// Distance as a percent of LaGeR string length
   float distance_pct;
 };
 
@@ -121,11 +127,13 @@ class GestureSubscriptionMessage {
  * Creates a message queue for subscribing to detected gesture notifications.
  */
 void CreateGestureSubscriptionQueue();
+
 /**
  * Constantly monitors the message queue and adds new subscriptions to a
  * vector.
  */
 void AddSubscribedGestures(vector<SubscribedGesture>* subscribed_gestures);
+
 /**
  * Blocking function that gets and returns a subscription message from the
  * queue.
@@ -133,10 +141,27 @@ void AddSubscribedGestures(vector<SubscribedGesture>* subscribed_gestures);
  * Code structure based on http://stackoverflow.com/a/12349823
  */
 GestureSubscriptionMessage GetGestureSubscriptionMessage();
+
+/**
+ * Sends a gesture subscription message to the standard registration queue.
+ */
 void SendGestureSubscriptionMessage(string gesture_name, string gesture_lager);
+
+/**
+ * Iterates through the gestures in a file and suscribes the calling process
+ * to their detection events.
+ */
 void SubscribeToGesturesInFile(string file_name);
 
+/**
+ * Blocking function that returns the latest detected gesture message in the
+ * current process' queue.
+ */
 DetectedGestureMessage GetDetectedGestureMessage();
+
+/**
+ * Sends a detected gesture message to a subscribed process' queue.
+ */
 void SendDetectedGestureMessage(string gesture_name, pid_t destination_pid);
 
 #endif /* LAGER_LIBLAGER_CONNECT_LIBLAGER_CONNECT_H */
