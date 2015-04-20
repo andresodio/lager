41a7c8acbe7a65be82975a54e051887e041e18c1
Andrés Odio <andresodio@gmail.com>
2015-04-20 01:51:13 +0100

    Common: Convert tabs to spaces in SphericalCoordinates code

--- common/spherical_coordinates.h
+++ common/spherical_coordinates.h
@@ -11,13 +11,13 @@
  * http://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
  */
 struct SphericalCoordinates {
-	int theta;
+  int theta;
   int phi;
 
-	bool operator==(const SphericalCoordinates &other) const
-	{
-		return (theta == other.theta && phi == other.phi);
-	}
+  bool operator==(const SphericalCoordinates &other) const
+  {
+    return (theta == other.theta && phi == other.phi);
+  }
 };
 
 /*
@@ -25,24 +25,24 @@ struct SphericalCoordinates {
  * http://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
  */
 namespace std {
-	template<>
-	/**
-	 * Structure that allows the use of the custom Spherical Coordinates class
-	 * as an unordered map key.
-	 */
-	struct hash<SphericalCoordinates>
-	{
-	  /**
-	   * Function call operator() overloading that computes a hash value for
-	   * SphericalCoordinates structures.
-	   */
-		std::size_t operator()(const SphericalCoordinates& k) const
-		{
-			// Compute individual hash values for mTheta and mPhi
-			// and combine them using XOR and bit shifting:
-			return ((hash<int>()(k.theta) ^ (hash<int>()(k.phi) << 1)));
-		}
-	};
+  template<>
+  /**
+   * Structure that allows the use of the custom Spherical Coordinates class
+   * as an unordered map key.
+   */
+  struct hash<SphericalCoordinates>
+  {
+    /**
+     * Function call operator() overloading that computes a hash value for
+     * SphericalCoordinates structures.
+     */
+    std::size_t operator()(const SphericalCoordinates& k) const
+    {
+      // Compute individual hash values for mTheta and mPhi
+      // and combine them using XOR and bit shifting:
+      return ((hash<int>()(k.theta) ^ (hash<int>()(k.phi) << 1)));
+    }
+  };
 }
 
 /**
@@ -50,7 +50,7 @@ namespace std {
  */
 double DegreesToRadians(double degrees)
 {
-	return (degrees/180 * M_PI);
+  return (degrees/180 * M_PI);
 }
 
 #endif /* LAGER_COMMON_SPHERICAL_COORDINATES_H */
