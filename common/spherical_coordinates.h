aad9dcdc2b0bdfca64bef7f6dc32e7d7becb1efc
Andrés Odio <andresodio@gmail.com>
2015-04-20 01:12:40 +0100

    Common: Add Doxygen for SphericalCoordinates code

--- common/spherical_coordinates.h
+++ common/spherical_coordinates.h
@@ -3,7 +3,10 @@
 
 #include <math.h>
 
-/*
+/**
+ * Structure that describes spherical coordinates, consisting of a theta
+ * (polar) angle and a phi (azimuthal) angle.
+ *
  * Code based on:
  * http://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
  */
@@ -23,8 +26,16 @@ struct SphericalCoordinates {
  */
 namespace std {
 	template<>
+	/**
+	 * Structure that allows the use of the custom Spherical Coordinates class
+	 * as an unordered map key.
+	 */
 	struct hash<SphericalCoordinates>
 	{
+	  /**
+	   * Function call operator() overloading that computes a hash value for
+	   * SphericalCoordinates structures.
+	   */
 		std::size_t operator()(const SphericalCoordinates& k) const
 		{
 			// Compute individual hash values for mPhi and mTheta
@@ -34,6 +45,9 @@ namespace std {
 	};
 }
 
+/**
+ * Converts degrees to radians.
+ */
 double DegreesToRadians(double degrees)
 {
 	return (degrees/180 * M_PI);
