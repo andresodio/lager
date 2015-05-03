11b2260ecbbe9031e739dc65adb62b8ae85fb0e1
Andrés Odio <andresodio@gmail.com>
2015-05-03 07:43:34 +0100

    Common: String Tokenizer - White space fixes

--- common/string_tokenizer.h
+++ common/string_tokenizer.h
@@ -10,24 +10,21 @@
  * Based on code from
  * http://oopweb.com/CPP/Documents/CPPHOWTO/Volume/C++Programming-HOWTO-7.html
  */
-void TokenizeString(const std::string& str,
-				std::vector<std::string>& tokens,
-				const std::string& delimiters)
-{
-	// Skip delimiters at beginning.
-	std::string::size_type last_pos = str.find_first_not_of(delimiters, 0);
-	// Find first "non-delimiter".
-	std::string::size_type pos     = str.find_first_of(delimiters, last_pos);
+void TokenizeString(const std::string& str, std::vector<std::string>& tokens,
+                    const std::string& delimiters) {
+  // Skip delimiters at beginning.
+  std::string::size_type last_pos = str.find_first_not_of(delimiters, 0);
+  // Find first "non-delimiter".
+  std::string::size_type pos = str.find_first_of(delimiters, last_pos);
 
-	while (std::string::npos != pos || std::string::npos != last_pos)
-	{
-		// Found a token, add it to the vector.
-		tokens.push_back(str.substr(last_pos, pos - last_pos));
-		// Skip delimiters.  Note the "not_of"
-		last_pos = str.find_first_not_of(delimiters, pos);
-		// Find next "non-delimiter"
-		pos = str.find_first_of(delimiters, last_pos);
-	}
+  while (std::string::npos != pos || std::string::npos != last_pos) {
+    // Found a token, add it to the vector.
+    tokens.push_back(str.substr(last_pos, pos - last_pos));
+    // Skip delimiters.  Note the "not_of"
+    last_pos = str.find_first_not_of(delimiters, pos);
+    // Find next "non-delimiter"
+    pos = str.find_first_of(delimiters, last_pos);
+  }
 }
 
 #endif /* LAGER_COMMON_STRING_TOKENIZER_H */
